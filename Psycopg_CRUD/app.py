40ab330c063029f4239e740bec0893758cb049a5
Seth Simmons <civeban@gmail.com>
2026-02-10 23:59:01 -0700

    added create table queries to db.py

--- Psycopg_CRUD/app.py
+++ Psycopg_CRUD/app.py
@@ -1,34 +1,10 @@
 from flask import Flask, jsonify, request
-from db import connect_db
+from db import connect_db, create_schema
 import routes
 
 
 conn = connect_db()
 cursor = conn.cursor()
-
-    # cursor.execute("""
-    #   CREATE TABLE IF NOT EXISTS Companies (
-    #   company_id SERIAL PRIMARY KEY,
-    #   company_name VARCHAR UNIQUE NOT NULL,
-    #   );
-    #   """)
-def create_categories():
-    print("Creating Table Categories...")
-    cursor.execute("""
-      CREATE TABLE IF NOT EXISTS Categories (
-      category_id SERIAL PRIMARY KEY,
-      category_name VARCHAR UNIQUE NOT NULL
-      );
-      """)
-    conn.commit()
-# def create_schema(cursor):
-
-#   with open('create_table.txt','r') as readfile:
-#     create_queries = readfile.read()
-
-#   cursor.executescript(create_queries)
-
-# result = create_schema(cursor)
 
 app = Flask(__name__)
 app.register_blueprint(routes.category)
@@ -37,5 +13,5 @@ app.register_blueprint(routes.product)
 app.register_blueprint(routes.warranty)
 
 if __name__ == '__main__':
-  create_categories()
+  result = create_schema()
   app.run()
--- Psycopg_CRUD/db.py
+++ Psycopg_CRUD/db.py
@@ -15,3 +15,67 @@ def connect_db():
 
   conn = psycopg2.connect(f"user={user_name} dbname={database_name}  host={app_host} port={app_port} password={password} ")
   return conn
+
+conn = connect_db()
+cursor = conn.cursor()
+
+def create_schema():
+  print('Creating Table: Companies')
+  cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Companies (
+      company_id SERIAL PRIMARY KEY,
+      company_name VARCHAR UNIQUE NOT NULL
+    );
+  """)
+  conn.commit()
+
+  print('Creating Table: Products')
+  cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Products (
+      product_id SERIAL PRIMARY KEY,
+      product_name VARCHAR UNIQUE NOT NULL,
+      company_id INTEGER,
+      description VARCHAR,
+      price REAL,
+      active BOOLEAN DEFAULT true,
+      FOREIGN KEY (company_id)
+        REFERENCES Companies(company_id)
+    );
+  """)
+  conn.commit()
+
+  print('Creating Table: Warranties')
+  cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Warranties (
+      warranty_id SERIAL PRIMARY KEY,
+      product_id INTEGER,
+      warranty_months VARCHAR NOT NULL,
+      FOREIGN KEY (product_id)
+        REFERENCES Products(product_id)
+    );
+  """)
+  conn.commit()
+
+  print('Creating Table: Categories')
+  cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Categories (
+      category_id SERIAL PRIMARY KEY,
+      category_name VARCHAR UNIQUE NOT NULL
+    );
+  """)
+  conn.commit()
+
+  print('Creating Table: ProductsCategoriesXref')
+  cursor.execute("""
+    CREATE TABLE IF NOT EXISTS ProductsCategoriesXref (
+      product_id INTEGER,
+      category_id INTEGER,
+      PRIMARY KEY (product_id, category_id),
+      FOREIGN KEY (product_id)
+        REFERENCES Products (product_id),
+      FOREIGN KEY (category_id)
+        REFERENCES Categories (category_id)
+    );
+  """)
+  conn.commit()
+
